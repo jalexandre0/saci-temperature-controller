d004ee7e9f4d7c5c654eb29444a31c137fc64325
Jefferson Alexandre dos Santos <jefferson.alexandre@gmail.com>
2017-02-05 08:11:07 -0200

    Low pass filter for temperature readings.

--- src/DS18B20.cpp
+++ src/DS18B20.cpp
@@ -7,13 +7,12 @@ float beerTemp() {
   static float _temp  ;
   tempSensor.requestTemperatures() ;
   //naive attempt to make a low pass filter
-  _beerTemp = (0.85 * _beerTemp ) + ( 0.15 * tempSensor.getTempCByIndex(0));
+  _beerTemp = (0.95 * _beerTemp ) + ( 0.05 * tempSensor.getTempCByIndex(0));
   return _beerTemp ;
 }
 
 float fridgeTemp() {
-  tempSensor.requestTemperatures() ;
-  _fridgeTemp = tempSensor.getTempCByIndex(1) ;
+  _fridgeTemp = (0.95 * _fridgeTemp ) + ( 0.05 * tempSensor.getTempCByIndex(1));
   return _fridgeTemp ;
 }
 
